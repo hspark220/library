7ed5d72ad70a01395b40eb406c7a6a0dcc0a1841
Heon Soo Park <hspark220@gmail.com>
2023-01-04 13:28:50 -0500

    Make looping for books in library

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 let myLibrary = [];
 
-function Book() {
+function Book(title, author) {
+    this.title = title
+    this.author = author
+}
 
+Book.prototype.getTitle = function() {
+    return this.title;
 }
 
 function addBookToLibrary() {
-    
+
 }
+
+// temporary manual books
+const book1 = new Book('book1', 'jasper');
+const book2 = new Book('book2', 'jasper');
+myLibrary.push(book1, book2);
+console.log(myLibrary);
+
+//Query selectors and elements
+const library = document.querySelector('.library');
+
+//printing the library
+for(let i = 0; i < myLibrary.length; i++) {
+    const book = document.createElement('div');
+    const bookTitle = document.createElement('p');
+    const bookAuthor = document.createElement('p');
+
+    book.classList.add('book');
+
+    book.append(bookTitle, bookAuthor);
+    library.appendChild(book);
+
+    bookTitle.textContent =  `${myLibrary[i].title}`;
+    bookAuthor.textContent = `${myLibrary[i].author}`; 
+}
+
