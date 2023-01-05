1626ded0e01b57787476494308dcc9af6ebb76cf
Heon Soo Park <hspark220@gmail.com>
2023-01-05 14:23:18 -0500

    Put inputs and adding button
    button is tied to addBook function
    also added print screen and clear screen function

--- script.js
+++ script.js
@@ -10,7 +10,14 @@ Book.prototype.getTitle = function() {
 }
 
 function addBookToLibrary() {
+    clearScreen();
 
+    const tempTitle = document.getElementById('input-title');
+    const tempAuthor = document.getElementById('input-author');
+    const tempBook = new Book(tempTitle.value, tempAuthor.value);
+    myLibrary.push(tempBook);
+    
+    printLibrary();
 }
 
 // temporary manual books
@@ -21,19 +28,33 @@ console.log(myLibrary);
 
 //Query selectors and elements
 const library = document.querySelector('.library');
+const addBookBtn = document.getElementById('addBook');
+const inputTitle = document.getElementById('input-title');
 
 //printing the library
-for(let i = 0; i < myLibrary.length; i++) {
-    const book = document.createElement('div');
-    const bookTitle = document.createElement('p');
-    const bookAuthor = document.createElement('p');
-
-    book.classList.add('book');
-
-    book.append(bookTitle, bookAuthor);
-    library.appendChild(book);
-
-    bookTitle.textContent =  `${myLibrary[i].title}`;
-    bookAuthor.textContent = `${myLibrary[i].author}`; 
+const printLibrary = () => {
+    for(let i = 0; i < myLibrary.length; i++) {
+        const book = document.createElement('div');
+        const bookTitle = document.createElement('p');
+        const bookAuthor = document.createElement('p');
+    
+        book.classList.add('book');
+    
+        book.append(bookTitle, bookAuthor);
+        library.appendChild(book);
+    
+        bookTitle.textContent =  `${myLibrary[i].title}`;
+        bookAuthor.textContent = `${myLibrary[i].author}`; 
+    }
 }
+
+const clearScreen = () => {
+    const oldBooks = document.querySelectorAll('.book');
+    oldBooks.forEach(oldBook => {
+        oldBook.remove();
+    });
+}
+
+
+addBookBtn.addEventListener('click', addBookToLibrary);
 
